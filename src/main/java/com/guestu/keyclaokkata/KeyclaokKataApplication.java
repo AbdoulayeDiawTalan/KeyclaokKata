0e3dfb8c9416ddf2a6a552dd4a8be1a10630d9ba
AbdoulayeDiaw <abdoulayediaw6@hotmail.com>
2025-01-13 16:16:26 +0100

    initial commit

---
+++ src/main/java/com/guestu/keyclaokkata/KeyclaokKataApplication.java
@@ -0,0 +1,13 @@
+package com.guestu.keyclaokkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyclaokKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyclaokKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guestu/keyclaokkata/KeyclaokKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestu.keyclaokkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyclaokKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
